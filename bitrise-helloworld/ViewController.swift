35d803451ac586022f90a2f22b6ae19b0dce68c1
nirifuji <nirifuji@healthcare.ne.jp>
2019-03-07 17:33:08 +0900

    Initial Commit

---
+++ bitrise-helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitrise-helloworld
+//
+//  Created by nirifuji on 2019/03/07.
+//  Copyright © 2019 nobukatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
